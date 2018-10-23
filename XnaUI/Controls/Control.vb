88f468b8192c01cfa3000f72b8937f9f91b78608
myluki2000 <lukas636@live.com>
2018-10-23 14:09:21 +0200

    Projektdateien hinzufügen.

---
+++ XnaUI/Controls/Control.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.Xna.Framework
+Imports Microsoft.Xna.Framework.Graphics
+
+Public Class Control
+    Public MustOverride Sub Draw(sb As SpriteBatch)
+    Public MustOverride Sub Update(gameTime As GameTime)
+
+End Class
---
+++ XnaUI/UIScreen.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.Xna.Framework
+Imports Microsoft.Xna.Framework.Graphics
+
+Public Class UIScreen
+    Public Controls As New List(Of Control)
+
+    Public Sub Draw(sb As SpriteBatch)
+        For Each c In Controls
+            c.Draw(sb)
+        Next
+    End Sub
+
+    Public Sub Update(gameTime As GameTime)
+        For Each c In Controls
+            c.Update(gameTime)
+        Next
+    End Sub
+End Class
